a4208a230604ad9f5fde62e6ef494d15ceae0c2f
kimjunbeom <hjhjaja1208@naver.com>
2024-06-19 14:50:34 +0900

    dfd

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hekllo') // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
